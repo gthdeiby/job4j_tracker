747d692396d0217cb2c8e6d0c43570ccf713693a
dim0nster <32572277+dim0nster@users.noreply.github.com>
2021-03-14 12:53:46 +0300

    1.1.1. Конструктор по умолчанию. [#298252]

--- src/main/java/ru/job4j/oop/Fix.java
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -3,11 +3,12 @@ package ru.job4j.oop;
 public class Fix {
     private String desc;
 
-    public Fix(String desc) {
-        this.desc = "Fix.";
+    public Fix(String text) {
+        this.desc = text;
     }
 
     public static void main(String[] args) {
-        Fix item = new Fix("Do not fix");
+        String solution = "Здрасьте";
+        Fix item = new Fix(solution);
     }
 }
