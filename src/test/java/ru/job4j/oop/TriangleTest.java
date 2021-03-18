af543dda3b5f980b852230dc5da4ed7bf4b64503
dim0nster <32572277+dim0nster@users.noreply.github.com>
2021-03-18 21:45:25 +0300

    2.2. Рефакторинг - Площадь треугольника. [#298242]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -8,20 +8,12 @@ import org.junit.Test;
 public class TriangleTest {
 
     @Test
-    public void period() {
+    public void area() {
         Point a = new Point(0, 0);
         Point b = new Point(4, 0);
         Point c = new Point(0, 4);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
-    }
-
-    @Test
-    public void exist() {
-    }
-
-    @Test
-    public void area() {
     }
 }
