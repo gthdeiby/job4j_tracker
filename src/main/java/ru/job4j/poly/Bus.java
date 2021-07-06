5ff0c779434df67cf669b6ac9711763f49318496
dim0nster <32572277+dim0nster@users.noreply.github.com>
2021-07-06 19:58:31 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void passengers(int quantity) {
+
+    }
+
+    @Override
+    public double refuel(double fuelAmount) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void go ();
+    void passengers (int quantity);
+    double refuel (double fuelAmount);
+}
