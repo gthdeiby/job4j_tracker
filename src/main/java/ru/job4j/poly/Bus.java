42f0599a4567ab2780f231b55903774c58b1c36a
dim0nster <32572277+dim0nster@users.noreply.github.com>
2021-07-07 22:10:55 +0300

    3. Интерфейс Input [#181092 #174902]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,18 +1,18 @@
 package ru.job4j.poly;
 
-public class Bus implements Transport{
+public class Bus implements Transport {
     @Override
     public void go() {
-
+        System.out.println("Ехать");
     }
 
     @Override
     public void passengers(int quantity) {
-
+        System.out.println("Едет" + quantity + "парссажира(ов)");
     }
 
     @Override
     public double refuel(double fuelAmount) {
-        return 0;
+        return fuelAmount * 300;
     }
 }
--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -1,7 +1,9 @@
 package ru.job4j.poly;
 
 public interface Transport {
-    void go ();
-    void passengers (int quantity);
-    double refuel (double fuelAmount);
+    void go();
+
+    void passengers(int quantity);
+
+    double refuel(double fuelAmount);
 }
