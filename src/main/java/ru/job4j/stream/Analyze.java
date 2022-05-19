ce4434295364baa7aab8c0f3e70ff8126c805177
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-05-19 07:38:25 +0300

    2. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -66,7 +66,6 @@ public class Analyze {
     public static Tuple bestSubject(Stream<Pupil> stream) {
         return stream.flatMap(p -> p.getSubjects().stream())
                 .collect(Collectors.groupingBy(Subject::getName,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::getScore)))
                 .entrySet().stream()
                 .map(s -> new Tuple(s.getKey(), s.getValue()))
